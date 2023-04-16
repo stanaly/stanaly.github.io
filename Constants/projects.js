383d05d2c0d13f513b658255f0b53206b1795f03
stanaly <tlsgusrb70@gmail.com>
2023-04-16 14:31:06 +0900

    feat: Change file to suit me

--- Constants/projects.js
+++ Constants/projects.js
@@ -1,41 +1,42 @@
 // import project demo images as follows.
-import PROJECT1_1 from '../styles/projects/Project1_1.jpg';
-import PROJECT1_2 from '../styles/projects/Project1_2.jpg';
-import PROJECT1_3 from '../styles/projects/Project1_3.jpg';
-import PROJECT2_1 from '../styles/projects/Project2_1.jpg';
-import PROJECT2_2 from '../styles/projects/Project2_2.jpg';
-import PROJECT2_3 from '../styles/projects/Project2_3.jpg';
-import PROJECT3_1 from '../styles/projects/Project3_1.jpg';
-import PROJECT3_2 from '../styles/projects/Project3_2.jpg';
-import PROJECT3_3 from '../styles/projects/Project3_3.jpg';
+import PROJECT1_1 from "../styles/projects/Project1_1.jpg";
+import PROJECT1_2 from "../styles/projects/Project1_2.jpg";
+import PROJECT2_1 from "../styles/projects/Project2_1.jpg";
+import PROJECT2_2 from "../styles/projects/Project2_2.jpg";
+import PROJECT2_3 from "../styles/projects/Project2_3.jpg";
+import PROJECT3_1 from "../styles/projects/Project3_1.jpg";
+import PROJECT3_2 from "../styles/projects/Project3_2.jpg";
 
 // first 3 projects are visible on the homepage while all are visible on the work page.
 
 export const projects = [
-    //leave the githublink field blank if it is a private repo or doent exist, alerts will take care of it
-    //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
-    {
-        name: 'Dunder Mifflin', //project name
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum', //project description
-        githubLink: '', //github repo link
-        projectLink: '', //deployed project link
-        tech: ['Tech1', 'Tech2', 'Tech3'], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
-        photo: [PROJECT1_1, PROJECT1_2, PROJECT1_3], //list of names of images from above imports.
-    },
-    {
-        name: 'Project 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
-        tech: ['Tech1', 'Tech2', 'Tech3'],
-        photo: [PROJECT2_1, PROJECT2_2, PROJECT2_3],
-    },
-    {
-        name: 'Project 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum',
-        githubLink: '/',
-        projectLink: '/',
-        tech: ['Tech1', 'Tech2', 'Tech3'],
-        photo: [PROJECT3_1, PROJECT3_2, PROJECT3_3],
-    },
-]
+  //leave the githublink field blank if it is a private repo or doent exist, alerts will take care of it
+  //leave the projectlink field blank if it the project has not been deployed, alerts will take care of it
+  {
+    name: "실습으로 배우는 SQL Injection", //project name
+    description:
+      "제주대학교 정보보안 동아리 'CrackUs'에서 부회장을 맡으며, '실습으로 배우는 SQL Injection' 책 작성과 실습 사이트 구현을 도왔습니다. 실습서 독자들을 위해 PHP와 MySQL로 간단한 사이트를 만들었고, CTF 문제들을 업로드하여 책에 내용을 쉽게 배울 수 있게 했습니다. 제주도에 부족한 보안 공부 인프라를 위해 책과 사이트를 홍보하며 제주 보안 커뮤니티 활성화에 기여하였습니다.", //project description
+    githubLink: "https://github.com/stanaly", //github repo link
+    projectLink: "/", //deployed project link
+    tech: ["보안", "개발", "동아리"], //techstack of the project, it is advisavble to mention only upto 3 technologies here to maintain the ui
+    photo: [PROJECT1_1, PROJECT1_2], //list of names of images from above imports.
+  },
+  {
+    name: "육군 사이버작전센터",
+    description:
+      "육군 사이버작전센터에서 CERT병으로 근무하며 육군 사이버보안 최전선을 지켰습니다. 주 업무는 보안사고 대응을 하였으며, 부분적으로 악성코드 분석과 네트워크 공격 분석 업무를 맡았습니다. 동기들과 대회에 참가하여 '화이트햇 콘테스트 합참의장상'과 '국방오픈소스아카데미 해커톤 육군참모총장상'을 수상했습니다.",
+    githubLink: "https://github.com/stanaly",
+    projectLink: "/",
+    tech: ["보안", "개발", "군복무"],
+    photo: [PROJECT2_1, PROJECT2_2, PROJECT2_3],
+  },
+  {
+    name: "팀 리드미",
+    description:
+      "제주 개발자 커뮤니티 활성화를 위해 제주대학교 개발 동아리 '리드미'를 개설하였습니다. 팀 리드미는 웹·AI·게임·보안 등 여러 분야에 관심 있는 사람들이 모여서 협업을 통해 프로젝트를 진행하고, 본인 관심 분야와 관련하여 글을 작성해서 발표하는 활동을 하고 있습니다. 저는 팀 인원들을 관리하고, 협업 가이드북 작성과 프로젝트 리뷰를 진행하고 있습니다. 또한, 개발 입문자들을 모아서 CS와 프로그래밍을 주제로 수업도 진행하고 있습니다.",
+    githubLink: "https://github.com/rleadme",
+    projectLink: "/",
+    tech: ["개발", "동아리", "운영"],
+    photo: [PROJECT3_1, PROJECT3_2],
+  },
+];
--- Constants/userinfo.js
+++ Constants/userinfo.js
@@ -1,125 +1,145 @@
 //PLEASE FOLLOW THE FORMAT OF THIS FILE.
 
-import { faGithubAlt, faLinkedinIn, faMediumM, faFacebook, faTwitter, faInstagram, faYoutube, faBehance } from "@fortawesome/free-brands-svg-icons"
+import {
+  faGithubAlt,
+  faLinkedinIn,
+  faMediumM,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faYoutube,
+  faBehance,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const userinfo = {
-    logoText: "Michael Scott", //This text is visible on your navbar and footer like your logo.
-    contact: {
-        email: 'michaelscott@email.com', //It is always a good idea to mention your email on your website. Good platform to communicate.
-        phone: '1234567890', //Phone number is optional, if you dont want it, consider leaving it blank .
-        countrycode: '+91' //It is advisable to add the country code incase you mention your contact number.
+  logoText: "현규의 공간", //This text is visible on your navbar and footer like your logo.
+  contact: {
+    email: "tlsgusrb70@email.com", //It is always a good idea to mention your email on your website. Good platform to communicate.
+    phone: "010-8766-9825", //Phone number is optional, if you dont want it, consider leaving it blank .
+    countrycode: "+82", //It is advisable to add the country code incase you mention your contact number.
+  },
+  socials: [
+    //For aesthetics, it is advisable for you to mention upto 4 social media links only. Fill in the links.
+    //more icons are imported above, use as you like them.
+    { type: "github", link: "https://github.com/stanaly", icon: faGithubAlt },
+    { type: "linkedin", link: "", icon: faLinkedinIn },
+    { type: "medium", link: "", icon: faMediumM },
+  ],
+  greeting: {
+    //this text goes on your landing page
+    title: "안녕하세요. 고민하는 개발자 신현규입니다.",
+    subtitle:
+      "블록체인과 웹해킹을 좋아하는 개발자입니다. 컴퓨터공학과 블록체인보안을 전공하고 있는 학생입니다. 목표를 설정하여 완수하는 것과 여러 사람들의 의견을 듣는 것을 좋아합니다. 문제를 마주할 때 고민을 하면서 본질을 파악하는 것을 추구합니다. 2023년부터 제주대학교 학생을 대상으로 개발 팀 ‘리드미’를 만들어 운영하고 있습니다. 현재는 스마트 계약과 DAO에 관심을 가지고 있습니다.",
+  },
+  capabilities: [
+    //this text goes on your home page in table format.
+    //For aesthetics, it is advisable for you to mention upto 4 catgeories links only.
+    //For aesthetics, it is advisable for you to keep similar number of skills in each category
+    {
+      category: "frontend", // eg.frontend, backend, devops etc
+      skills: ["React", "Angular", "Vue"], //eg. react, html, python etc.
     },
-    socials: [
-        //For aesthetics, it is advisable for you to mention upto 4 social media links only. Fill in the links.
-        //more icons are imported above, use as you like them.
-        { type: 'github', link: '', icon: faGithubAlt },
-        { type: 'linkedin', link: '', icon: faLinkedinIn },
-        { type: 'medium', link: '', icon: faMediumM },
+    {
+      category: "backend",
+      skills: ["Express", "Django", "Spring"],
+    },
+    {
+      category: "blockchain",
+      skills: ["Ethereum", "EOSIO", "Hyperledger Fabric"],
+    },
+    {
+      category: "hacking",
+      skills: ["Web", "Network", "System"],
+    },
+  ],
+  about: {
+    //this text goes at the bottom of your home page.
+    content:
+      "제주도에서 자라며 어린 나이부터 컴퓨터에 관심이 많았습니다. 주변에 개발 학원이나 커뮤니티가 없어서 일단 혼자 무작정 C언어를 공부하였습니다. 메모리 접근과 비트 연산 등 이해하기 어려운 지식들이 많아서 간단한 개념도 이해하는데 정말 많은 시간을 써야만 했습니다. 공부 방법도 방향도 모르는 상태로 컴퓨터와 관련된 지식들은 모두 흡수하려고 했습니다. 대학교 입학 전까지 실속없는 공부만 하다가 학교에서 좋은 선배들을 만나 공부 방향을 잡을 수 있었고, 빠르게 성장할 수 있었습니다. 선배들 덕분에 점점 더 전문적인 경험을 할 수 있었고, 주변에 실력자들도 많이 알게 되었습니다. 저는 유년기에 겪었던 일들을 다른 사람들도 겪지 않았으면 하는 마음에 선배들에게 받은 도움을 후배들에게, 더 나아가 어려움을 겪고 있는 제주도 개발 입문자들에게 주고 싶습니다. 아직 저도 많이 부족하지만, 서로 알려주고 도와줄 수 있는 활발한 제주 개발자 커뮤니티를 만드는데 기여하고 싶습니다.",
+    resume: "/", //link your resume here. it can be drive link or any other link
+  },
+  education: {
+    visible: true, //set this to false if you want to omit this section
+    educationList: [
+      {
+        time: "2019년 03월", //timespan
+        title: "대학교 입학", //eg. BTech in Compuster Engineering
+        organization: "1학년", //eg. VJTI, Mumbai
+        description:
+          "정보보안 동아리 활동을 중심으로 해커톤 참가, 주니어 논문 작성, 고등학교 멘토링, CTF 실습 사이트 제작 등 신입생의 패기로 최대한 많은 경험을 하기 위해 노력했습니다.", //eg. secured a cgpa of 9.25 in all sems appeared in so far
+      },
+      {
+        time: "2021년 07월",
+        title: "군입대 ",
+        organization: "육군",
+        description:
+          "대규모 네트워크를 관리하면서 많은 경험을 할 수 있었고, 실력이 좋은 동기들과 해킹방어대회와 SW해커톤을 참가하면서 세상을 보는 시야를 넓힐 수 있었습니다.",
+      },
+      {
+        time: "2022년 01월",
+        title: "대학교 복학",
+        organization: "3학년",
+        description:
+          "정보보안 전문가의 꿈을 접고, 블록체인 개발자가 되기 위해 공부하고 있습니다. 또한, 제주도 개발자 커뮤니티 활성화를 목표로 제주대학교 개발 동아리를 만들어 운영하고 있습니다.",
+      },
     ],
-    greeting: {
-        //this text goes on your landing page
-        title: "Hey there, I am Michael.",
-        subtitle: "Michael Gary Scott is a fictional character in NBC's The Office, portrayed by Steve Carell. Michael is the protagonist of the series, serving as the Regional Manager of the Scranton branch of a paper distribution company, Dunder Mifflin Inc. Also, that's what she said!"
-    },
-    capabilities: [
-        //this text goes on your home page in table format.
-        //For aesthetics, it is advisable for you to mention upto 4 catgeories links only.
-        //For aesthetics, it is advisable for you to keep similar number of skills in each category
-        {
-            category: "Category", // eg.frontend, backend, devops etc
-            skills: ["Skillset1", "Skillset1", "Skillset1"] //eg. react, html, python etc.
-        },
-        {
-            category: "Category",
-            skills: ["Skillset2", "Skillset2", "Skillset2"]
-        },
-        {
-            category: "Category",
-            skills: ["Skillset3", "Skillset3", "Skillset3"]
-        },
-        {
-            category: "Category",
-            skills: ["Skillset4", "Skillset4", "Skillset4"]
-        },
+  },
+  experience: {
+    visible: false, //set this to false if you want to omit this section
+    experienceList: [
+      {
+        company: "Dunder Mifflin", //company name eg.Microsoft
+        companylogo:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*", //companylogo
+        position: "Regional Manager", //post you held eg.Senior SDE
+        time: "March 2020 - May 2020", //timespan
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum", //eg. spearheaded the graphics team in designing SASS product UIs
+      },
+      {
+        company: "Dunder Mifflin",
+        companylogo:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*",
+        position: "Regional Manager",
+        time: "March 2020 - May 2020",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum",
+      },
+      {
+        company: "Dunder Mifflin",
+        companylogo:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*",
+        position: "Regional Manager",
+        time: "March 2020 - May 2020",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum",
+      },
     ],
-    about: {
-        //this text goes at the bottom of your home page.
-        content: "'Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked. But it’s not like this compulsive need like my need to be praised.' Whatever the situation or conflict might be, it can almost always be traced back to this need. His conference room characters, fun runs, and copious amounts of office parties are all aimed at his need to be praised.",
-        resume: "/" //link your resume here. it can be drive link or any other link
-    },
-    education: {
-        visible: true, //set this to false if you want to omit this section
-        educationList: [
-            {
-                time: 'May - Oct 2020', //timespan
-                title: 'Degree name', //eg. BTech in Compuster Engineering
-                organization: 'School Name', //eg. VJTI, Mumbai
-                description: 'Lorem ipsum dolor sit amet consectetur' //eg. secured a cgpa of 9.25 in all sems appeared in so far
-            },
-            {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
-            },
-            {
-                time: 'May - Oct 2020',
-                title: 'Degree name',
-                organization: 'School Name',
-                description: 'Lorem ipsum dolor sit amet consectetur'
-            },
-        ],
-    },
-    experience: {
-        visible: true, //set this to false if you want to omit this section
-        experienceList: [
-            {
-                company: 'Dunder Mifflin', //company name eg.Microsoft
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*', //companylogo
-                position: 'Regional Manager', //post you held eg.Senior SDE
-                time: 'March 2020 - May 2020', //timespan
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum' //eg. spearheaded the graphics team in designing SASS product UIs
-            },
-            {
-                company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
-                position: 'Regional Manager',
-                time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
-            },
-            {
-                company: 'Dunder Mifflin',
-                companylogo: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/why-did-steve-carell-leave-the-office-1585597666.jpg?crop=0.563xw:0.844xh;0.287xw,0.128xh&resize=480:*',
-                position: 'Regional Manager',
-                time: 'March 2020 - May 2020',
-                description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
-            },
-        ],
-    },
-    blogs: {
-        //set this to false if you want to omit this section
-        visible: true
-    },
-}
+  },
+  blogs: {
+    //set this to false if you want to omit this section
+    visible: false,
+  },
+};
 
 export const headings = {
-    //you can customise all the headings here.
-    workHomePage: 'Work',
-    workMainPage: 'Projects',
-    capabilities: 'Capabilities',
-    about: 'About Me',
-    education: 'Education',
-    experience: 'Experiences',
-    blogs: 'I write!',
-    contact: 'Contact Me',
-}
+  //you can customise all the headings here.
+  workHomePage: "저는 경험을 좋아합니다.",
+  workMainPage: "경험",
+  capabilities: "기술 스택",
+  about: "저는",
+  education: "히스토리",
+  experience: "Experiences",
+  blogs: "I write!",
+  contact: "문의 하기",
+};
 
 export const ctaTexts = {
-    //you can customise all the cta texts here.
-    landingCTA: 'My work',
-    workCTA: 'View All',
-    capabCTA: 'Get in Touch',
-    educationCTA: 'About Me',
-    resumeCTA: 'Resume',
-    submitBTN: 'Submit'
-}
+  //you can customise all the cta texts here.
+  landingCTA: "활동 보기",
+  workCTA: "전체 보기",
+  capabCTA: "연락 하기",
+  educationCTA: "저는",
+  resumeCTA: "위로 가기",
+  submitBTN: "보내기",
+};
